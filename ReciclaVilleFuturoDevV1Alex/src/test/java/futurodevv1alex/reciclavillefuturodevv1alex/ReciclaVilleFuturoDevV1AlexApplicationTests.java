f68ddb8d475f9a9dfccfc31e458e2aa3db83bf75
alexritzmann <alexritzmann@gmail.com>
2025-05-05 21:58:30 -0300

    Primeiro Commit - Criação do Projeto

---
+++ ReciclaVilleFuturoDevV1Alex/src/test/java/futurodevv1alex/reciclavillefuturodevv1alex/ReciclaVilleFuturoDevV1AlexApplicationTests.java
@@ -0,0 +1,13 @@
+package futurodevv1alex.reciclavillefuturodevv1alex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaVilleFuturoDevV1AlexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
